cce78c26ed6a515146835c8115ce473eb7fa48e3
nikitagordeev10 <nikitagordeev10@yandex.ru>
2024-10-24 23:15:37 +0300

    оформил для github

---
+++ FrequencyAnalysisTask.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalysis {
+    static class FrequencyAnalysisTask {
+        //
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text) {
+            //
+            var unSort = new Dictionary<string, Dictionary<string, int>>();
+            var result = new Dictionary<string, string>();
+            var firstWords = new StringBuilder();
+
+            // Встречаемость слов по отдельности 
+            unSort = CreateDictionary(text, unSort, 1);
+            
+            // Встречаемость биграм
+            var forSort = CreateDictionary(text, unSort, 2);
+            
+            // 
+            return SortDictionary(forSort, result, firstWords);
+        }
+
+        //
+        public static Dictionary<string, Dictionary<string, int>> CreateDictionary(List<List<string>> text, Dictionary<string, Dictionary<string, int>> unSort, int a) {
+            //
+            foreach (var sentence in text) {
+                for (var i = 0; i < sentence.Count - a; i++) {
+                    var bond = "";
+                    var val = "";
+                    //
+                    if (a == 2) {
+                        bond = sentence[i] + " " + sentence[i + 1];
+                        val = sentence[i + 2];
+                    } else {
+                        bond = sentence[i];
+                        val = sentence[i + 1];
+                    }
+                    //
+                    if (unSort.ContainsKey(bond))
+                        if (unSort[bond].ContainsKey(val))
+                            unSort[bond][val]++;
+                        else
+                            unSort[bond][val] = 1;
+                    else
+                        unSort[bond] = new Dictionary<string, int> { { val, 1 } };
+                }
+            }
+            return unSort;
+        }
+
+        // Объединяю биграмы и триграмы в один список nграм
+        public static Dictionary<string, string> SortDictionary(Dictionary<string, Dictionary<string, int>> forSort, Dictionary<string, string> result, StringBuilder firstWords) {
+            // 
+            var maxValue = 0;
+            var endWord = "";
+            //
+            foreach (var pair in forSort) {
+                //
+                firstWords.Append(pair.Key);
+                //
+                foreach (var value in forSort[pair.Key]) {
+                    if (value.Value > maxValue) {
+                        maxValue = value.Value;
+                        endWord = value.Key;
+                    } else if (value.Value == maxValue)
+                        
+                        // лексикографическое сравнения
+                        if (string.CompareOrdinal(value.Key, endWord.ToString()) < 0)
+                            endWord = value.Key;
+                }
+                //
+                result.Add(firstWords.ToString(), endWord.ToString());
+                maxValue = 0;
+                endWord = "";
+                firstWords.Clear();
+            }
+            //
+            return result;
+        }
+    }
+}
+
+
+
+/*
+ * Подсказки
+ * В этой задаче важно придумать декомпозицию всей задачи на 3-4 подзадачи. 
+ * Первая подзадача — построить частотный словарь: начало N-граммы → последнее слово N-граммы → количество повторов этой N-граммы
+ * Вторая подзадача — по началу N-граммы выбирать самое частное продолжение.
+ * Третья подзадача — собрать результаты остальных подзадач в решение всей задачи.
+ */
+
+/* 
+ * Материалы:
+ * String.CompareOrdinal Метод // Microsoft URL: https://learn.microsoft.com/ru-Ru/dotnet/api/system.string.compareordinal?view=net-5.0 (дата обращения: 18.12.2022).
+ * Улучшить код поиска частотности словосочетаний // cyberforum URL: https://www.cyberforum.ru/csharp-beginners/thread2348594.html (дата обращения: 18.12.2022).
+ * Частотность N-грамм // pastebin URL: https://pastebin.com/dmw8KCzY (дата обращения: 18.12.2022).
+ * Проблема с памятью // stackoverflow URL: https://ru.stackoverflow.com/questions/1037316/Проблема-с-памятью (дата обращения: 18.12.2022).
+ * Dictionary<TKey,TValue>.ContainsKey(TKey) Метод // Microsoft URL: https://learn.microsoft.com/ru-ru/dotnet/api/system.collections.generic.dictionary-2.containskey?view=net-6.0 (дата обращения: 18.12.2022).
+ * Проблема с памятью // StackOverflow URL: https://ru.stackoverflow.com/questions/1037316/Проблема-с-памятью (дата обращения: 18.12.2022).
+ * Двойной словарь // StackOverflow URL: https://ru.stackoverflow.com/questions/827941/Двойной-словарь (дата обращения: 18.12.2022).
+ */
---
+++ SashaKrasnov.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+namespace TextAnalysis {
+    static class FrequencyAnalysisTask {
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text) {
+            Dictionary<string, int> cheeze3 = RepairGram(AllGrams(text));
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            if (text.Count == 0) return result;
+            foreach (var item in cheeze3) {
+                string[] keysSplited = item.Key.ToString().Split(':');
+                result.Add(keysSplited[0], keysSplited[1]);
+            }
+            return result;
+        }
+        static Dictionary<string, int> AllGrams(List<List<string>> text) {
+            Dictionary<string, int> book = new Dictionary<string, int>();
+            List<string> twoGrams = new List<string>();
+            if (text.Count == 0) return book;
+            for (int i = 0; i < text.Count; i++) {
+                for (int j = 0; j < text[i].Count - 1; j++) {
+
+                    twoGrams.Add(text[i][j] + ':' + text[i][j + 1]);
+
+                }
+            }
+            for (int i = 0; i < twoGrams.Count; i++) {
+                if (book.ContainsKey(twoGrams[i])) book[twoGrams[i]] += 1;
+                else book.Add(twoGrams[i], 1);
+            }
+            List<string> threeGrams = new List<string>();
+            for (int i = 0; i < text.Count; i++) {
+                if (text[i].Count > 2) {
+                    for (int j = 0; j < text[i].Count - 2; j++) {
+
+                        threeGrams.Add(text[i][j] + ' ' + text[i][j + 1] + ':' + text[i][j + 2]);
+
+                    }
+                }
+            }
+            for (int i = 0; i < threeGrams.Count; i++) {
+                if (book.ContainsKey(threeGrams[i])) book[threeGrams[i]] += 1;
+                else book.Add(threeGrams[i], 1);
+            }
+            return book;
+        }
+        static Dictionary<string, int> RepairGram(Dictionary<string, int> text) {
+            if (text.Count < 2) return text;
+            foreach (var value1 in text.ToList()) {
+                string[] values1 = value1.Key.Split(':');
+                foreach (var value2 in text.ToList()) {
+                    string[] values2 = value2.Key.Split(':');
+                    if (values1[0] == values2[0]) {
+                        if (value2.Value > value1.Value) text.Remove(value1.Key);
+                        else if (value2.Value < value1.Value) text.Remove(value2.Key);
+                        else if (String.Compare(values1[0] + values1[1], values2[0] + values2[1]) < 0) text.Remove(value2.Key);
+                        else if (String.Compare(values1[0] + values1[1], values2[0] + values2[1]) > 0) text.Remove(value1.Key);
+                    }
+                }
+            }
+            return text;
+        }
+    }
+}
